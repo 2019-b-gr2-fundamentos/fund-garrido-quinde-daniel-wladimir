d40b7c23afba5fe56a1781dfb4e0399c4593bf3b
Adriana-Enriquez <adriana.enriquez@epn.edu.ec>
2019-10-21 15:52:11 -0500

    VARIABLES PRIMITIVAS

---
+++ 03/variables en java.js
@@ -0,0 +1,22 @@
+//variables primitivas
+const numero = 10;
+const decimaL = 10.12;
+const respuestaTipoNumero = typeof numero;
+console.log(respuestaTipoNumero);
+console.log(typeof decimaL);
+//Gestor de paquetes
+// de nodejs
+// Gestor de paquetes
+// de nodejs comunicarse con el repositorio y descargar el "paquete"
+// npm--> yarn
+const letra = "a";
+const palabra = "palabra";
+console.log(typeof palabra);
+console.log(typeof letra);
+//Variables primitivas number y string
+const hijos = "null";
+console.log(typeof null);
+const casado = false;
+console.log(typeof false);
+const fecha = new Date();
+console.log(typeof fecha);
