0d47c40112eed78b20e607ab320b6b375fce94ac
Daniel Garrido <daniel.garriod@epn.edu.ec>
2019-10-23 17:55:44 -0500

    04

---
+++ 04/numeros y strings.ts
@@ -0,0 +1,38 @@
+const numeroUno = 1; // camelCase
+const numero_dos = 2;
+let resultado = 0;
+//SUMA
+resultado = 1 + 2;
+// RESTAR
+resultado = 1 - 2;
+// MULTIP´LICACION
+resultado = 1 * 2;
+// DIVIDIR
+resultado = 1 / 2;
+// EXPONENCIAL
+resultado = 1 ^ 2;
+// RESTAR
+resultado = 1-2;
+// MODD
+resultado = 4%2;//0---> reSSIDO
+resultado = 4 % 3;//1-->RESIDUO
+
+resultado = 7 % 3; // 
+console.log("pera" + 2);
+
+
+
+
+
+
+
+
+
+
+
+
+//NUMEROUNO-> TODO MAYUS (SEPARACION NO)
+//NUMERO_UNO-> mayusculas guion bajo
+//NumeroUno->
+//numerouno-> python-->
+//numero_uno->
