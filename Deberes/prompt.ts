8d0fc5caa0fff7d22139c7b4b42eabfef99caf4f
22 <11>
2019-10-28 16:02:27 -0500

    HI

--- Deberes/prompt.ts
+++ Deberes/prompt.ts
@@ -2,6 +2,11 @@ const numeroUnoString = prompt("ingresa el numero 1"); //Devuleve el string
 const numeroDosString = prompt("ingresa el numero 2");
 const numeroUno = Number(numeroUnoString);
 const numeroDos = Number(numeroDosString);
+const operador1 = prompt("suma");
+
+
+
+
 console.log(numeroUno + numeroDos);
 console.log(numeroUno - numeroDos);
 console.log(numeroUno * numeroDos);
