afa6d63d9fee859ad4ac136e1ce1729305c27304
22 <11>
2019-10-28 16:00:12 -0500

    HI

---
+++ 05/condicionales.ts
@@ -0,0 +1,55 @@
+//Condicionales 
+const casado = true ;
+if (casado == true){
+  console.log("Si estoy casado");
+} else {
+    console.log("No estoy casado");
+}
+
+if (casado != false){  // Expresion --> Booleano    // con el != ponemos "distinto
+    console.log("No estoy casado");
+} else {
+    console.log("Si estoy casado");
+}
+
+
+const tengoMosa = false;
+const casadoYMosero = casado ==true && tengoMosa ==true;
+if(casadoYMosero){
+    console.log("Casado y mosero");
+} else{
+    console.log("O casado o Mosero o Nada");
+}
+
+//or 
+//true y true == true
+//true y false ==true
+//false y true == true
+//false y false == false
+// con -->||<-- se une todo 
+
+
+
+
+
+
+
+
+
+/*
+//apaga la primera alarma
+//bolita y lloro hasta segunda
+//alarma y me levanto
+//celularDescargado
+//amaneceMuerto
+//estaAcrivadaLaAlarma != true
+//estaEnModoSilencio
+//const seAtrasoAClase == true || apaga la primera alarma == true || bolitaYLloroHastaSegunda == true || alarmaYMeLevanto == true ||celularDescargado == true ||amaneceMuerto == true ||estaAcrivadaLaAlarma != true||estaEnModoSilencio==true
+si se apaga la primera alarma o me hago bolita o el celular esta descargado o amenece muerto o no esta activada la alarma o el celular esta en modo silencio se atraso
+
+
+
+
+
+
+*/
---
+++ Deberes/prompt.ts
@@ -0,0 +1,11 @@
+const numeroUnoString = prompt("ingresa el numero 1"); //Devuleve el string
+const numeroDosString = prompt("ingresa el numero 2");
+const numeroUno = Number(numeroUnoString);
+const numeroDos = Number(numeroDosString);
+console.log(numeroUno + numeroDos);
+console.log(numeroUno - numeroDos);
+console.log(numeroUno * numeroDos);
+console.log(numeroUno / numeroDos);
+/*
+En las lineas del 1 al 5 transformamos un string en un numero
+*/
