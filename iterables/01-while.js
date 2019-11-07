34f4bf4734524e57f0481327ac5d68f5024b3277
DanM10 <daniel.garrido.w8@hotmail.com>
2019-11-07 00:42:01 -0600

    fail solucionado
    ejercicio de cantidad de deberes en analisis algebra etc

--- iterables/01-while.js
+++ iterables/01-while.js
@@ -36,9 +36,57 @@ do {
             break;
     }
 }
+/*
+1 Analisis
+2 Algebra
+3 Compiladores
+5 programacion
+7 calculo
+11 fisica
+13 ingles
 
 
-
-
-const deberes = 38
-console.log(deberes%13);
+Si es inpar aumentar a analisis socioeconomico
+si es par aumentamos a algebra
+si es multiplo de 3 aumentamos a compiladores
+*/
+var numero = 1;
+var Analisis = 0;
+var Algebra = 0;
+var Compiladores = 0;
+var programacion = 0;
+var calculo = 0;
+var fisica = 0;
+var ingles = 0;
+do {
+    numero = numero + 1;
+    if ((numero % 13) == 0) {
+        ingles = ingles + 1;
+    }
+    else {
+        if ((numero % 11) == 0) {
+            fisica = fisica + 1;
+        }
+        else {
+            if (numero % 7 == 0) {
+                calculo = calculo + 1;
+            }
+            else {
+                if (numero % 5 == 0) {
+                    programacion = programacion + 1;
+                }
+                else {
+                    if (numero % 3 == 0) {
+                        Algebra = Algebra + 1;
+                    }
+                    else {
+                        if (numero % 1 == 0) {
+                            Analisis = Analisis + 1;
+                        }
+                    }
+                }
+            }
+        }
+    }
+} while (numero == 1000);
+console.log("la cantida de deberes de ingles fue " + fisica);
--- iterables/01-while.ts
+++ iterables/01-while.ts
@@ -57,7 +57,7 @@ si es par aumentamos a algebra
 si es multiplo de 3 aumentamos a compiladores
 */
 
-let numero = 0;
+let numero = 1;
 let Analisis = 0; 
 let Algebra = 0;
 let Compiladores = 0;
@@ -68,22 +68,35 @@ let ingles = 0;
 
 
 do{
-numero = numero + 1 
-if (numero%13 == 0){
-    ingles ++  
+  numero = numero + 1
+      if ((numero%13) == 0){
+          ingles = ingles + 1   
 }else{
-  if(numero%)
+      if((numero%11) == 0){
+        fisica = fisica + 1
+  }else{
+       if(numero%7 == 0){
+          calculo = calculo + 1
+    }else{
+        if(numero%5 == 0){
+        programacion = programacion + 1
+      }else{
+        if(numero%3 == 0){
+          Algebra = Algebra + 1
+        }else{
+        if(numero%1 == 0){
+            Analisis = Analisis + 1
+          }
+        }
+      }
+    }
+  }
 }
-} while (numero == 1000)
 
-console.log(deberes de ingles )
-Analisis = Analisis + 1
-Algebra = Algebra + 1
-Compiladores = Compiladores + 1
-programacion = programacion + 1
-calculo = calculo + 1
-fisica - fisica + 1
-ingles = ingles +1
+} while (numero <= 1000);
+
+console.log(`la cantida de deberes de ingles fue ${fisica}`);
+
 
 
 
