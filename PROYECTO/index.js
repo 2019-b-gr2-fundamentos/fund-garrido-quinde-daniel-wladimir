81cc386d5834bb9ecc9751721ecd345002a12706
Kevin-ramos <55715422+Kevin-ramos@users.noreply.github.com>
2020-01-09 17:42:39 -0500

    yeih

--- PROYECTO/index.js
+++ PROYECTO/index.js
@@ -8,7 +8,29 @@ import = imporat
 * = todo
 as = como
 from= de
+
+
+ejemplo
+si en el archivo suma.js de la LIBRERIA existiera mas de un module.exports
+les ponemos nombres tal que
+module.exports.a
+module.exports.loromero
+module.exports.c
+
+y despues  en lugar del asterisco nosotros utilizarmes corchetes y colocaremos las funciones a exportar tq
+import {a,loromero} as nombrexD from ....direccion
+
+
+las livrerias no las debemos verisonalizar en el github
+por eso usamos el git.ignore
+
+recuerda tambien usamos proyecto pilas para el informe
+proyecto no se sube al internet, libreria si
+
+npm install ---> va a correr el .json del PROYECTO e isntalara la libreria
 */
-var Sumalib = require('lib-garrido2019/lib/sacar3ang');
-var respuesta = Sumalib(45, 45);
+var piii = require('lib-garrido2019/lib/pi');
+var Sumalib = require('lib-garrido2019/lib/area')
+var respuesta = Sumalib(5,8)
 console.log('la respuesta es :', respuesta);
+console.log('el valor de pi es :', piii);
--- Proyecto advance/index.js
+++ Proyecto advance/index.js
@@ -1,3 +1,3 @@
-var Sumalib = require("libreria-deber/lib/resta");
+var Sumalib = require('lib-garrido2019/lib/sacar3ang');
 var respuesta = Sumalib(4, 5);
 console.log('la respuesta es :', respuesta);
